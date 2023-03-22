563cfb1594df14dfa08545af39099806d1323e7c
yxs <2719118523@qq.com>
2023-03-22 12:02:10 +0800

    commit

---
+++ SpringBoot/src/main/java/com/springboot/integratedWebFramework/entity/Student.java
@@ -0,0 +1,19 @@
+package com.springboot.integratedWebFramework.entity;
+
+import lombok.Data;
+
+/**
+ * @author YXS
+ * @PackageName: com.springboot.integratedWebFramework.entity
+ * @ClassName: Student
+ * @Desription:
+ * @date 2023/3/22 11:56
+ */
+@Data
+public class Student {
+
+    Integer sid;
+    String name;
+    String sex;
+
+}
